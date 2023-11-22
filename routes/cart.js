1e49274f55b9d9bfed82aab03c8abe168555c8f1
SJY <jun000405@naver.com>
2023-11-22 18:27:50 +0900

    feat: css 수정 및 ejs 수정

---
+++ routes/cart.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+// /cart => GET
+router.get('/cart', );
+
+// /cart/delete-cart-product => POST
+router.post('/cart/delete-cart-product', );
+
+// /cart/update-count-product => POST
+router.post('/cart/update-count-product', );
+
+exports.routes = router;
